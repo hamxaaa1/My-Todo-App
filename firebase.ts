8a354be9a0d7fff1536c61fea4d74f77eaac8660
Hamza Ashfaq <145859648+hamxaaa1@users.noreply.github.com>
2026-01-06 23:21:49 +0500

    v1

--- firebase.ts
+++ firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDLYN_YenedAi0vpUgh97s76BLcoXL2L38",
-  authDomain: "my-notes-app-cfe84.firebaseapp.com",
-  projectId: "my-notes-app-cfe84",
-  storageBucket: "my-notes-app-cfe84.firebasestorage.app",
-  messagingSenderId: "979780344418",
-  appId: "1:979780344418:web:a49ec8ae964a76bcf88f3b",
-  measurementId: "G-NNMFCZTF4H"
+  apiKey: "AIzaSyDLYN_YenedAi0vpUgh97s76BLcoXL3X12",
+  authDomain: "my-notees-app-cfe84.firebaseapp.com",
+  projectId: "my-notees-app-cfe24",
+  storageBucket: "my-notees-app-cfe14.firebasestorage.app",
+  messagingSenderId: "979780312414",
+  appId: "1:979781354711:web:a49ec8ae964a76bcf88f3b",
+  measurementId: "G-NNMFCMTF9H"
 };
 
 // Initialize Firebase
